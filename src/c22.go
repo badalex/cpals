6638b1878ee57c969e37dcfadee3437c4fa89dca
Alex Hunsaker <badalex@gmail.com>
2014-10-18 22:27:03 -0600

    cpals: fix minor thinko, take out debugging in c21

--- src/c22.go
+++ src/c22.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"cpals/mt"
-	"fmt"
+	//	"fmt"
 	"os"
 	"runtime"
 	"time"
@@ -18,25 +18,26 @@ func main() {
 	// wait a random number of seconds
 	sleepM.Seed(uint(time.Now().Unix() ^ int64(os.Getpid())))
 	s := time.Duration(sleepM.Rand()%maxSleep) * time.Second
-	fmt.Printf("pretend sleeping: %d\n", s/time.Second)
+	//fmt.Printf("pretend sleeping: %d\n", s/time.Second)
 
 	// seeds the rng with the current unix timestamp
 	seed := uint(time.Now().Unix()) + uint(s/time.Second)
-	fmt.Printf("seed is %d\n", seed)
+	//fmt.Printf("seed is %d\n", seed)
 	m := mt.MT{}
 	m.Seed(seed)
 
 	// wait a random number of seconds
 	s = time.Duration(sleepM.Rand()%maxSleep) * time.Second
-	fmt.Printf("pretend sleeping: %d\n", s/time.Second)
+	s = 0
+	//fmt.Printf("pretend sleeping: %d\n", s/time.Second)
 
 	// get the first 32 bits
 	r := m.Rand()
 
 	// figure out the seed we now that it had to been seeded in the last couple
 	// of minute so just brute it
-	nunix := seed + uint(s/time.Second)
-	nseed := nunix - (maxSleep + 1)
+	nunix := seed + uint(s/time.Second) + 1
+	nseed := nunix - (maxSleep)
 
 	prog := make(chan uint)
 	got := make(chan uint)
@@ -59,8 +60,9 @@ func main() {
 
 	for {
 		select {
-		case gseed := <-got:
-			fmt.Printf("found seed %d\n", gseed)
+		//case gseed := <-got:
+		case _ = <-got:
+			//fmt.Printf("found seed %d\n", gseed)
 			return
 		case _ = <-timeout:
 			panic("failed to find seed, timedout")
